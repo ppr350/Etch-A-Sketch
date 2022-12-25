e62eee9ab0f5731c513282fd727daa84bdeb58a7
ppr350 <kobito610@gmail.com>
2022-12-25 21:04:31 +0000

    Add script.js style.css

--- script.js
+++ script.js
@@ -1,44 +1,77 @@
 /*-------create the container for all square div------*/
-const container = document.createElement("div");
+let container = document.createElement("div");
 container.id = "container";
 document.body.appendChild(container);
 
 /*--------------------create grids--------------------*/
 
-function createGrids (grids) {
-    for (let y = 0; y < grids; y++) {
-        const makeGrids = document.createElement("div");
-        makeGrids.className = "makeGrids";
-        document.getElementById("container").appendChild(makeGrids);
+function createDefaultGrid(gridNum) {
+    for (let x = 0; x < (gridNum * gridNum); x++) {
+        const grid = document.createElement("div");
+        grid.className = "grid";
+        document.getElementById("container").appendChild(grid);
+        container.style.gridTemplateRows = `repeat(${gridNum}, 1fr)`
+        container.style.gridTemplateColumns = `repeat(${gridNum}, 1fr)`
+        console.log("make default grids successfully.")
     }
 }
-createGrids(256);
+createDefaultGrid(16)
 
 /*-----------------User's custom grids----------------*/
-//let getCustomGridNumbers = document.querySelector("#get-custom-grid-numbers");
 
-let clear = document.querySelector("#get-custom-grid-numbers");
-clear.addEventListener("click", () => {
-    resetGrids()
+let clearBtn = document.querySelector("#get-custom-grid-numbers");
+clearBtn.addEventListener("click", () => {
+    resetGrids();
+    console.log("button behaves normally.")
+    getUserGridNum();
+    console.log("get user grid input successfully.");
+    setUserDefineGrid();
 })
 
+/*----------------erase current grids---------------*/
 
-let setCustomGridNumber = 0;
+function resetGrids() {
+    let container = document.getElementById("container");
+    while (container.firstChild) {
+        container.removeChild(container.firstChild);
+        console.log("erase all existing grids successfully.");
+    }
+}
+
+/*------------get user input grid number------------*/
+
+let userGridNum = 0;
 isValid = false;
-function setCustomGridNumberfuntion() {
+function getUserGridNum() {
     do {
-        setCustomGridNumber = prompt("Number of square per side? Maximum 100 please.");
-        if (setCustomGridNumber === null) break //User clicked "CANCEL"
+        userGridNum = prompt("Number of square per side? Maximum 100 please.");
+        if (userGridNum === null) break //User clicked "CANCEL"
 
-        setCustomGridNumber = parseInt(setCustomGridNumber);
-        isValid = setCustomGridNumber >= 1 && setCustomGridNumber <= 100;
+        userGridNum = parseInt(userGridNum);
+        isValid = userGridNum >= 1 && userGridNum <= 100;
 
         if (isValid) {
-            alert("You chose " + setCustomGridNumber);
+            alert("You chose " + userGridNum);
         } else {
             alert("Number must be at no less than 0 and no more than 100 please.");
         }
     } while ( !isValid );
-    console.log(setCustomGridNumber);
+    container.style.gridTemplateRows = `repeat(${userGridNum}, auto)`
+    container.style.gridTemplateColumns = `repeat(${userGridNum}, auto)`
 }
+
+/*-----------make custom grid from user input--------*/
+
+function setUserDefineGrid() {
+    for (let x = 0; x < (userGridNum * userGridNum); x++) {
+        const grid = document.createElement("div");
+        grid.className = "grid";
+        document.getElementById("container").appendChild(grid);
+        console.log(userGridNum)
+    }
+    console.log("user grids set out successfully.")
+}
+
+/*-------------make a mouse down function-------------*/
+
 
